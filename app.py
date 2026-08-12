daae63d56fecc162b6244e1edf564eed93a7628e
MOHAMED IRFAN J <jsirfan9319@gmail.com>
2026-08-12 19:12:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-print("Hello from my AWS DevOps Docker application!")
-print("GitHub -> Jenkins -> Docker CI pipeline is working!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from my AWS DevOps Docker application! GitHub -> Jenkins -> Docker -> AWS EC2 is working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
