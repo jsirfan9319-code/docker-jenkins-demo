e686418821288e19a6186c3117ba789a756b02bc
MOHAMED IRFAN J <jsirfan9319@gmail.com>
2026-08-13 20:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Hello from AWS DevOps! Version 2 - Automatic CI/CD deployment is working!"
+  return "Hello from AWS DevOps! Version 3 - Automatic deployment is working!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
