f98e1e9d5c23987cbc34d949835c68cdaaf9bc08
Ubuntu <ubuntu@ip-172-31-8-188.ap-south-1.compute.internal>
2026-08-17 13:52:17 +0000

    Update Flask app dependencies

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total number of requests"
+)
+
 @app.route("/")
 def home():
-  return "Hello from AWS DevOps!Version 6 - Automatic deployment is working!"
+    REQUEST_COUNT.inc()
+    return "Hello from AWS DevOps! Version 2 - Automatic CI/CD deployment is working!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
